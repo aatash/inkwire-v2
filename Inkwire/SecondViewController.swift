a662db6f8c5a7a22b062002252c21c4422afae2c
Aatash Parikh <aatash.parikh@gmail.com>
2018-07-02 11:08:30 -0700

    Initial Commit

---
+++ Inkwire/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Inkwire
+//
+//  Created by Aatash Parikh on 7/2/18.
+//  Copyright © 2018 Metaphor Educaiton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
